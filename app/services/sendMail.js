89c6cb49dd6c567c4312d7afc486d70cd847e32a
patrickletourneux <patrick.letourneux20@gmail.com>
2022-04-11 14:08:42 +0200

    send mail succeded ,see sendMail.js

--- app/services/sendMail.js
+++ app/services/sendMail.js
@@ -2,30 +2,33 @@ const nodemailer = require('nodemailer');
 const debug = require('debug')('sendMail');
 
 const sendMail = async function (receverEmail, sub, mes) {
-    const transporter = nodemailer.createTransport({
-        streamTransport: true,
-        newline: 'windows'
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure : false,
+        auth: {
+            user: process.env.PATRICKMAIL,
+            pass: process.env.PATRICKMAILPASS,
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false,
+          },
     });
 
-    var message = {
-        from: 'patriste92@hotmail.fr',
-        to: receverEmail,
-        subject: sub,
-        text: mes,
-        html: '<p>Please confirm your email</p>'
+    let mailOptions = {
+        from: 'cduprops',
+        to: "amelieforgeard@gmail.com",
+        subject: `fais ton menage , tu es en retard !!!!`,
+        html: `tu as 35 points de retard`,
     };
 
-    try {
-        await transporter.sendMail(message, (error, info) => {
-            debug('Message sent: %s', message);
-            debug('info.messageId:', info.messageId);
-            return (info.messageId);
-        })
-    } catch (error) {
-        debug(error);
-        return (error);
-    };
-    debug('end send mail');
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            debug('error ', err);
+        } else {
+            debug('info ',info);
+        }
+    });
 }
 
 module.exports = sendMail;
